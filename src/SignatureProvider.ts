c8f0ce9e990f8c445960425639d83616e68d9375
Shaq Kheder <shaq@cryptomechanics.com>
2019-10-11 13:15:57 -0400

    Support for custom and multiple indices with caching and backwards compatibility

--- src/SignatureProvider.ts
+++ src/SignatureProvider.ts
@@ -6,12 +6,14 @@ interface SignatureProviderInterface {
   eosjsApi: Api
   ledgerApi: LedgerAPI
   cachedKeys: string[]
+  cachedKeysFull: Array<{ indexNumber: number, key: string }>
 }
 
 export class SignatureProvider implements SignatureProviderInterface {
   public eosjsApi: Api = null
   public ledgerApi: LedgerAPI = null
   public cachedKeys: string[] = []
+  public cachedKeysFull: Array<{ indexNumber: number, key: string }> = []
 
   public async getLedgerApi() {
     if (this.ledgerApi) {
@@ -24,15 +26,20 @@ export class SignatureProvider implements SignatureProviderInterface {
   }
 
   /** Public keys associated with the private keys that the `SignatureProvider` holds */
-  public async getAvailableKeys(requestPermission?: boolean) {
-    if (this.cachedKeys.length) {
-      return this.cachedKeys
-    }
-
+  public async getAvailableKeys(requestPermission?: boolean, indexArray?: number[]) {
     try {
       const api = await this.getLedgerApi()
-      const key = await api.getPublicKey(requestPermission)
-      this.cachedKeys = [key]
+      if (!indexArray || !indexArray.length) {indexArray = [0]}
+      const keys = this.cachedKeysFull.slice()
+      for (const indexNumber of indexArray) {
+        const cached = keys.find((key) => key.indexNumber === indexNumber)
+        if (!cached) {
+          const key = await api.getPublicKey(requestPermission, indexNumber)
+          keys.push({indexNumber, key})
+        }
+      }
+      this.cachedKeysFull = keys
+      this.cachedKeys = this.cachedKeysFull.map((key) => key.key)
       return this.cachedKeys
     } catch (error) {
       throw error
@@ -40,10 +47,12 @@ export class SignatureProvider implements SignatureProviderInterface {
   }
 
   /** Sign a transaction */
-  public async sign({ chainId, serializedTransaction }: { chainId: string, serializedTransaction: Uint8Array }) {
+  public async sign(
+    { chainId, serializedTransaction, indexNumber}:
+    { chainId: string, serializedTransaction: Uint8Array, indexNumber?: number }) {
     try {
       const api = await this.getLedgerApi()
-      const signatures = await api.signTransaction({ chainId, serializedTransaction })
+      const signatures = await api.signTransaction({ chainId, serializedTransaction, indexNumber })
       return { signatures, serializedTransaction }
     } catch (error) {
       throw error
